59375fe4b95c55dbf2e00c82124e63d2d93af590
sivapoornima <sivapoornimakaruturi@gmail.com>
2018-05-23 10:38:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RoutingModule } from './/routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+const appRoutes:Routes = 
+[
+     {path:"" , component:HomeComponent},
+     {path:"about" , component:AboutComponent},
+     {path:"contact" , component:ContactComponent}
+]
+
+@NgModule({
+  imports: [ 
+    CommonModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  declarations: [],
+  exports:[RouterModule]
+})
+export class RoutingModule { }
